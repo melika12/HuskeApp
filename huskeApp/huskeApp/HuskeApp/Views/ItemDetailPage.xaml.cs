f761976c3bb5e93d9c2dfc281b2022a89d5c91c4
Melika Pedersen <melika.pedersen@r2p.com>
2021-11-19 09:52:56 +0100

    init commit

---
+++ huskeApp/huskeApp/HuskeApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HuskeApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HuskeApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
